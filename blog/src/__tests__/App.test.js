b6b3001b3ac28bb9467589a6b69758f3ec9925df
Faizaan <dkfaizaan12@gmail.com>
2023-09-30 05:03:56 -0700

    Tests for navbar

--- blog/src/__tests__/App.test.js
+++ blog/src/__tests__/App.test.js
@@ -1,5 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import App from '../App';
+import { Nav } from '../components/header/Nav';
 
 describe("",()=>{
   it('renders the navbar', () => {
@@ -14,10 +15,38 @@ describe("",()=>{
     expect(logoElement).toBeInTheDocument();
   });
 
-  it('renders the links', () => {
+  it('checks list length', () => {
     render(<App />);
     const linksElement = screen.getAllByRole('link');
     expect(linksElement.length).toBe(5); //home,featured,about,contact,createpost
+  });
+
+  //links check
+  it('navigates to home page', () => {
+    render(<Nav />);
+    const homeLink = screen.getByText('Home');
+    fireEvent.click(homeLink);
+    expect(window.location.pathname).toBe('/');
+  });
+
+  it('navigates to about page', () => {
+    render(<Nav />);
+    const aboutLink = screen.getByText('About');
+    fireEvent.click(aboutLink);
+    expect(window.location.pathname).toBe('/about');
+  });
+  it('navigates to about page', () => {
+    render(<Nav />);
+    const aboutLink = screen.getByText('Featured');
+    fireEvent.click(aboutLink);
+    expect(window.location.pathname).toBe('/featured');
+  });
+
+  it('navigates to contact page', () => {
+    render(<Nav />);
+    const contactLink = screen.getByText('Contact');
+    fireEvent.click(contactLink);
+    expect(window.location.pathname).toBe('/contact');
   });
 
 })
