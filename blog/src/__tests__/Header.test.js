c35d4c207239c3ee008e9f92a52476b41e7811ea
Faizaan <dkfaizaan12@gmail.com>
2023-10-02 12:33:40 +0500

    used screen object/resolved other issues

--- blog/src/__tests__/Header.test.js
+++ blog/src/__tests__/Header.test.js
@@ -1,4 +1,4 @@
-import { render} from '@testing-library/react';
+import { render,fireEvent, screen} from '@testing-library/react';
 import Header from '../components/Header/Header';
 import { MemoryRouter } from 'react-router-dom';
 
@@ -6,17 +6,16 @@ import { MemoryRouter } from 'react-router-dom';
 describe("Unit tests for Header",()=>{
     
   it('Updates input value on change in search bar', () => {
-    const {getByTestId}=render(<MemoryRouter><Header/></MemoryRouter>);
-    const inputSearch=getByTestId('inputSearch');
-
-    fireEvent.change(inputTitle,{target:{value:"test"}})
+   render(<MemoryRouter><Header/></MemoryRouter>);
+    const inputSearch=screen.getByTestId('inputSearch');
+    fireEvent.change(inputSearch,{target:{value:"test"}})
     expect(inputSearch.value).toBe("test")
   
   });
 
   it('Number of tags should be 3', () => {
-    const { getAllByTestId } =render(<MemoryRouter><Header/></MemoryRouter>);
-    const tagElements = getAllByTestId('tag');
+    render(<MemoryRouter><Header/></MemoryRouter>);
+    const tagElements = screen.getAllByTestId('tag');
     expect(tagElements.length).toBe(3);
   });
 
