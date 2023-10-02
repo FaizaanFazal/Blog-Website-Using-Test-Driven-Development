4e265b615f8c54cc5526ddb4b28001b9833ad48c
Faizaan <dkfaizaan12@gmail.com>
2023-10-02 13:15:47 +0500

    added some tests/snapshot test

--- blog/src/__tests__/Header.test.js
+++ blog/src/__tests__/Header.test.js
@@ -4,13 +4,23 @@ import { MemoryRouter } from 'react-router-dom';
 
 
 describe("Unit tests for Header",()=>{
+
+  it('Snapshot test for rendering navbar in header',()=>{
+    const { asFragment } = render(<MemoryRouter><Header/></MemoryRouter>);
+    expect(asFragment()).toMatchSnapshot();
+  })
+
+  it('Dom test for-> renders the navbar', () => {
+    render(<MemoryRouter><Header/></MemoryRouter>);
+    const navbarElement = screen.getByRole('navigation');
+    expect(navbarElement).toBeInTheDocument();
+  });
     
   it('Updates input value on change in search bar', () => {
    render(<MemoryRouter><Header/></MemoryRouter>);
     const inputSearch=screen.getByTestId('inputSearch');
     fireEvent.change(inputSearch,{target:{value:"test"}})
     expect(inputSearch.value).toBe("test")
-  
   });
 
   it('Number of tags should be 3', () => {
@@ -18,5 +28,15 @@ describe("Unit tests for Header",()=>{
     const tagElements = screen.getAllByTestId('tag');
     expect(tagElements.length).toBe(3);
   });
+
+  it('Checks if button is disabled if search bar is empty',()=>{
+    render(<MemoryRouter><Header/></MemoryRouter>);
+    const searchbtn = screen.getByText('Search');
+    const inputSearch=screen.getByTestId('inputSearch');
+    fireEvent.change(inputSearch, { target: { value: '' } });
+    expect(searchbtn).toBeDisabled()
+  })
+
+ 
 
 })
