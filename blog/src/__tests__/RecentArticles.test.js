dce181f5e89e551ce6472b26e34656e9e421892e
Faizaan <dkfaizaan12@gmail.com>
2023-10-03 17:34:57 +0500

    test for card link

--- blog/src/__tests__/RecentArticles.test.js
+++ blog/src/__tests__/RecentArticles.test.js
@@ -2,7 +2,21 @@ import { render, screen} from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 import RecentArticles from '../components/RecentArticles/RecentArticles';
 import Card from '../components/RecentArticles/Card';
+import images from '../utils/images';
 
+const small = [ //will be in utils folder later
+    {
+        id: 1,
+        title: "10 Tips for Budget-Friendly Travel",
+        author: "Elena Rodriguez",
+        authorImage: images.author1,
+        date: "15 August",
+        image: images.blogImg1,
+        verified:true,    
+        slug:'post1', 
+        content: "test Traveling on a budget doesn't have to compromise your experience. Here are 10 tips to make the most of your journey without breaking the bank."
+    }
+]
 
 describe("Unit tests for RecentArticle Components",()=>{
 
@@ -15,6 +29,13 @@ describe("Unit tests for RecentArticle Components",()=>{
     render(<MemoryRouter><Card/></MemoryRouter>);
     const image = screen.getByTestId('Image');
     expect(image).toBeInTheDocument();
+
+  });
+
+  it('Card link has right slug', () => {
+    render(<MemoryRouter><Card blogItemData = {small} isCardSm = { true } key = {small.id}/></MemoryRouter>);
+    const slugLink = screen.getByTestId('slugLink');
+    expect(slugLink).toHaveAttribute('href', '/blogs/post1')
 
   });
     
