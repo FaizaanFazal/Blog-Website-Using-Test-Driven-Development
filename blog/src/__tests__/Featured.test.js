bbe04bdd490a5c9c8b0abff92efc6eeed3c1f85e
Faizaan <dkfaizaan12@gmail.com>
2023-10-04 15:42:20 +0500

    update:removed unneccesary usestates/tests wrapped in provider

--- blog/src/__tests__/Featured.test.js
+++ blog/src/__tests__/Featured.test.js
@@ -1,7 +1,7 @@
-import { render, screen, within} from '@testing-library/react';
+import {  screen, within} from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 import Featured from '../components/Featured/Featured';
-
+import { renderWithProviders }  from '../utils/wrappertesting';
 import {isProperImageURL,small,greater} from '../utils/helperfunctions'
 import CardWide from '../components/Featured/CardWide';
 
@@ -9,18 +9,18 @@ import CardWide from '../components/Featured/CardWide';
 describe("Unit tests for Featured component",()=>{
   
   it('Dom test for rendering Featured', () => {
-    render(<MemoryRouter><Featured/></MemoryRouter>);
+    renderWithProviders(<MemoryRouter><Featured/></MemoryRouter>);
     const featureElement = screen.getByRole('featured');
     expect(featureElement).toBeInTheDocument();
   });
 
   it('snapshot test to check Featured component', () => {
-    const { asFragment } = render(<MemoryRouter><Featured/></MemoryRouter>);
+    const { asFragment } = renderWithProviders(<MemoryRouter><Featured/></MemoryRouter>);
     expect(asFragment()).toMatchSnapshot();
   });
 
   it('Image renders and src is correct', () => {
-    render(<MemoryRouter><Featured/></MemoryRouter>);
+    renderWithProviders(<MemoryRouter><Featured/></MemoryRouter>);
     const image = screen.getByTestId('FeaturedImg');
     expect(image).toBeInTheDocument();
     const imageSrc = image.src;
@@ -28,39 +28,39 @@ describe("Unit tests for Featured component",()=>{
   });
 
   it('Image Alternative (alt) is correct', () => {
-    render(<MemoryRouter><Featured/></MemoryRouter>);
+    renderWithProviders(<MemoryRouter><Featured/></MemoryRouter>);
     const Featuredimage = screen.getByTestId('FeaturedImg');
     expect(Featuredimage).toHaveAttribute('alt', 'Featured')
   });
 
   it('Heading is rendered', () => {
-    render(<MemoryRouter><Featured/></MemoryRouter>);
+    renderWithProviders(<MemoryRouter><Featured/></MemoryRouter>);
     const heading = screen.getByTestId('headingfeatured');
     expect(heading).toBeInTheDocument();
   });
 
   it('Summary  with greater than 250 chars',async () => {
-    render(<MemoryRouter><CardWide blogItemData = { greater[0]} /></MemoryRouter>);
+    renderWithProviders(<MemoryRouter><CardWide blogItemData = { greater[0]} /></MemoryRouter>);
     const summaryG = screen.getByTestId('featureSummary');
     const anchorTagElementsG =  within(summaryG).queryAllByTestId('slugLink');
     expect(anchorTagElementsG.length).toBe(1);
   });
 
   it('Summary with less than 250 chars', () => {
-    render(<MemoryRouter><CardWide blogItemData = { small} /></MemoryRouter>);
+    renderWithProviders(<MemoryRouter><CardWide blogItemData = { small} /></MemoryRouter>);
     const summarys = screen.getByTestId('featureSummary');
     const anchorTagElements = within(summarys).queryAllByTestId('slugLink');
     expect(anchorTagElements.length).toBe(0);
   });
 
   it('Date is rendered', () => {
-    render(<MemoryRouter><Featured/></MemoryRouter>);
+    renderWithProviders(<MemoryRouter><Featured/></MemoryRouter>);
     const featureDate = screen.getByTestId('featureDate');
     expect(featureDate).toBeInTheDocument();
   });
 
   it('Check view more slug link to be correct', () => {
-    render(<MemoryRouter><CardWide blogItemData = { greater[0]} /></MemoryRouter>);
+    renderWithProviders(<MemoryRouter><CardWide blogItemData = { greater[0]} /></MemoryRouter>);
     const slugLink = screen.getByTestId('slugLink');
     expect(slugLink).toHaveAttribute('href', '/blogs/post1')
   });
--- blog/src/components/Featured/CardWide.jsx
+++ blog/src/components/Featured/CardWide.jsx
@@ -1,44 +1,41 @@
 import images from "../../utils/images";
 import "../../styles/Card.scss";
 import PropTypes from "prop-types";
-import { useState } from "react";
 import { Link } from 'react-router-dom';
 
 
 const CardWide = ( {blogItemData} ) => {
     const MAX_LENGTH = 250;
-    const [featured,setFeatured]=useState(blogItemData);
-    const [summary,setSummary]=useState(blogItemData.content)
    
   return (
     <div className="card-grid grid">
       <div className="card-grid-img">
-        <img data-testid='FeaturedImg' src = {`${featured.image}`} alt = "Featured" className="object-fit-cover" />
+        <img data-testid='FeaturedImg' src = {`${blogItemData.image}`} alt = "Featured" className="object-fit-cover" />
       </div>
       <div className="card-grid-text">
         <div>
           <div className="badge badge-primary">featured</div>
           <h2 data-testid='headingfeatured' className="title-lg title featured-title">
-            { featured.title }
+            { blogItemData.title }
           </h2>
           
-            {summary?.length > MAX_LENGTH ?
+            {blogItemData.content?.length > MAX_LENGTH ?
             <p className="text-lg text" data-testid='featureSummary'>
-                    {`${featured.content.substring(0, MAX_LENGTH)}...`}<Link data-testid='slugLink' to={`/blogs/${featured.slug}`}>Read more</Link>
+                    {`${blogItemData.content.substring(0, MAX_LENGTH)}...`}<Link data-testid='slugLink' to={`/blogs/${blogItemData.slug}`}>Read more</Link>
             </p>
             :
-            <p data-testid='featureSummary'>{featured.content}</p>
+            <p data-testid='featureSummary'>{blogItemData.content}</p>
             }
         </div>
 
         <div className="card-footer flex justify-between items-center">
             <div className="writer-info grid">
                 <div className="info-avatar">
-                <img src = { `${featured.authorImage }`} alt="" className="object-fit-cover" />
+                <img src = { `${blogItemData.authorImage }`} alt="" className="object-fit-cover" />
                 </div>
                 <div className="info-intro">
-                    <p className="intro-name text-base">{ featured.author }</p>
-                    {featured.verified ? 
+                    <p className="intro-name text-base">{ blogItemData.author }</p>
+                    {blogItemData.verified ? 
                       <div className="intro-verify flex items-center">
                           <img src = { `${images.verify_icon_filled}`} alt="icon" className="verify-icon" />
                           <span className="verify-status text-sm text">Verified writer</span>
@@ -51,7 +48,7 @@ const CardWide = ( {blogItemData} ) => {
                     }
                 </div>
             </div>
-            <div className="date-info text text-base" data-testid='featureDate'>{ featured.date}</div>
+            <div className="date-info text text-base" data-testid='featureDate'>{ blogItemData.date}</div>
         </div>
       </div>
     </div>
