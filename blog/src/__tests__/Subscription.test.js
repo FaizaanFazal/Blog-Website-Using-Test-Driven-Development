7edb51aac712544a0c899bf201782584a0543029
Faizaan <dkfaizaan12@gmail.com>
2023-10-09 12:27:25 +0500

    Fix: resolved all linters

--- blog/src/__tests__/Subscription.test.js
+++ blog/src/__tests__/Subscription.test.js
@@ -5,35 +5,32 @@ import { renderWithProviders } from '../utils/wrappertesting';
 import Subscription from '../components/Subscription/Subscription';
 
 describe('Unit tests for Subscription Component', () => {
+  it('snapshot test to check it renders Subscription component', () => {
+    const { asFragment } = renderWithProviders(<MemoryRouter><Subscription /></MemoryRouter>);
+    expect(asFragment()).toMatchSnapshot();
+  });
 
-    it('snapshot test to check it renders Subscription component', () => {
-        const { asFragment } = renderWithProviders(<MemoryRouter><Subscription /></MemoryRouter>);
-        expect(asFragment()).toMatchSnapshot();
-    });
+  it('Image renders in Subscription component', () => {
+    renderWithProviders(<MemoryRouter><Subscription /></MemoryRouter>);
+    const image = screen.getByTestId('Image');
+    expect(image).toBeInTheDocument();
+  });
 
-    it('Image renders in Subscription component', () => {
-        renderWithProviders(<MemoryRouter><Subscription /></MemoryRouter>);
-        const image = screen.getByTestId('Image');
-        expect(image).toBeInTheDocument();
-    });
-    
-    it('Form renders in component', () => {
-        renderWithProviders(<MemoryRouter><Subscription /></MemoryRouter>);
-        const formelement = screen.getByTestId('form');
-        expect(formelement).toBeInTheDocument();  
-    });
+  it('Form renders in component', () => {
+    renderWithProviders(<MemoryRouter><Subscription /></MemoryRouter>);
+    const formelement = screen.getByTestId('form');
+    expect(formelement).toBeInTheDocument();
+  });
 
-    it('Email input changes on input', () => {
-        renderWithProviders(<MemoryRouter><Subscription /></MemoryRouter>);
-        const email = screen.getByTestId('email');
-        fireEvent.change(email, { target: { value: 'test' } });
-        expect(email.value).toBe('test');
-    });
-    it('Button renders in subscitpion component', () => {
-        renderWithProviders(<MemoryRouter><Subscription /></MemoryRouter>);
-        const subscribebtn = screen.getByTestId('subscribebtn');
-        expect(subscribebtn).toBeInTheDocument();  
-    });
-
+  it('Email input changes on input', () => {
+    renderWithProviders(<MemoryRouter><Subscription /></MemoryRouter>);
+    const email = screen.getByTestId('email');
+    fireEvent.change(email, { target: { value: 'test' } });
+    expect(email.value).toBe('test');
+  });
+  it('Button renders in subscitpion component', () => {
+    renderWithProviders(<MemoryRouter><Subscription /></MemoryRouter>);
+    const subscribebtn = screen.getByTestId('subscribebtn');
+    expect(subscribebtn).toBeInTheDocument();
+  });
 });
-
