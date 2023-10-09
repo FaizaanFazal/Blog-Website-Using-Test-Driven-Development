29d7ca4adc1d491f9650c5875d99490703ce1aed
Faizaan <dkfaizaan12@gmail.com>
2023-10-09 19:37:08 +0500

    Update: All tests pass/ resolved all linters

--- blog/src/components/Footer/Footer.jsx
+++ blog/src/components/Footer/Footer.jsx
@@ -8,7 +8,7 @@ export default function Footer() {
       <div className="container">
         <div className="ftr-list">
           <div className="ftr-item">
-            <a href="#" className="ftr-brand text-white">
+            <a href="/" className="ftr-brand text-white">
               Blogs
               {' '}
               <span className="ftr-brand-dot bg-white" />
@@ -18,16 +18,18 @@ export default function Footer() {
               Build a modern and creative website with crealand
             </p>
             <ul className="social-links text-white flex flex-wrap text-gray text-base">
-              <li><a data-testid="social" href="/">Twitter</a></li>
-              <li><a data-testid="social" href="/">Linkedin</a></li>
-              <li><a data-testid="social" href="/">Instagram</a></li>
-              <li><a data-testid="social" href="/">Google</a></li>
-              <li><a data-testid="social" href="/">YouTube</a></li>
+              <li><a data-testid="social" href="https://github.com/jsx-eslint">Twitter</a></li>
+              <li><a data-testid="social" href="https://github.com/jsx-eslint">Linkedin</a></li>
+              <li><a data-testid="social" href="https://github.com/jsx-eslint">Instagram</a></li>
+              <li><a data-testid="social" href="https://github.com/jsx-eslint">Google</a></li>
+              <li><a data-testid="social" href="https://github.com/jsx-eslint">YouTube</a></li>
             </ul>
           </div>
-
-          <FooterLink props={linksData} />
-
+          {
+            linksData.map((col) => (
+              <FooterLink key={col.id} linkData={col} />
+            ))
+          }
         </div>
         <div data-testid="copyright" className="ftr-text text-center text-gray text-base">Copyright &copy; 2023. Crafted with love.</div>
       </div>
--- blog/src/components/Footer/FooterLink.jsx
+++ blog/src/components/Footer/FooterLink.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
 
-const FooterLink = ({ linksData }) => (
-  <div className="ftr-item">
-    <h4 className="ftr-item-title text-lg">{ linksData.title }</h4>
-    <ul className="ftr-nav-links">
-      {
-        linksData.links.map((link, index) => (
-          <li className="ftr-nav-item" key={`link${index}`}>
-            <a data-testid="otherLinks" href="/" className="ftr-nav-link text-base">{ link }</a>
-          </li>
-        ))
+export default function FooterLink({ linkData }) {
+  return (
+    <div className="ftr-item">
+      <h4 className="ftr-item-title text-lg">{ linkData.title }</h4>
+      <ul className="ftr-nav-links">
+        {
+          linkData.links.map((item) => (
+            <li className="ftr-nav-item" key={item.id}>
+              <a data-testid="otherLinks" href={item.url} className="ftr-nav-link text-base">{ item.name }</a>
+            </li>
+          ))
         }
-    </ul>
-  </div>
-);
-
-export default FooterLink;
+      </ul>
+    </div>
+  );
+}
