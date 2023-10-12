98f00fe31d0ed5ca79aa6cbad30c2033ccacecc2
Faizaan <dkfaizaan12@gmail.com>
2023-10-12 03:55:20 -0700

    Update: added test/resolved linters

--- blog/src/__tests__/CreatePost.test.js
+++ blog/src/__tests__/CreatePost.test.js
@@ -1,12 +1,13 @@
 import { fireEvent, screen } from '@testing-library/react';
 import { renderWithProviders } from '../utils/wrappertesting';
-import CreatePost from "../components/CreatePost/CreatePost"
+import CreatePost from '../components/CreatePost/CreatePost';
+import { isProperImageURL } from '../utils/helperfunctions';
 
 describe('Unit tests for CreatePost component', () => {
   it('Dom test for rendering CreatePost', () => {
     renderWithProviders(<CreatePost />);
-    const featureElement = screen.getByTestId('createpost');
-    expect(featureElement).toBeInTheDocument();
+    const createElement = screen.getByTestId('createpost');
+    expect(createElement).toBeInTheDocument();
   });
 
   it('snapshot test to check CreatePost component', () => {
@@ -34,4 +35,18 @@ describe('Unit tests for CreatePost component', () => {
     expect(Submitbtn).toBeInTheDocument();
   });
 
+  it('Image renders and src is correct', () => {
+    renderWithProviders(<CreatePost />);
+    const imgInput = screen.getByTestId('imgInput');
+    expect(imgInput).toBeInTheDocument();
+    const imageSrc = imgInput.src;
+    expect(isProperImageURL(imageSrc)).toBe(true);
+  });
+
+  it('Updates input value on change in slug', () => {
+    renderWithProviders(<CreatePost />);
+    const inputslug = screen.getByTestId('inputslug');
+    fireEvent.change(inputslug, { target: { value: 'test' } });
+    expect(inputslug.value).toBe('test');
+  });
 });
