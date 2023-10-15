d5085a49fd5ee85bc5bca00492b91d8122bf7dba
Faizaan <dkfaizaan12@gmail.com>
2023-10-15 06:54:06 -0700

    Added: tests for utility helper functions

--- blog/src/__tests__/Helperfunctions.test.js
+++ blog/src/__tests__/Helperfunctions.test.js
@@ -1,4 +1,4 @@
-import { isProperImageURL } from '../utils/helperfunctions';
+import { isProperEmail, isProperImageURL, isProperPass } from '../utils/helperfunctions';
 
 describe('Tests for isProperImageURL functions', () => {
   test('isProperImageURL-> returns false for incorrect URL', () => {
@@ -7,5 +7,26 @@ describe('Tests for isProperImageURL functions', () => {
 
   test('isProperImageURL-> returns true for correct URL', () => {
     expect(isProperImageURL('https://example.com/image.jpg')).toBeTruthy();
+  });
+});
+
+describe('Tests for isProperEmail function', () => {
+  test('isProperEmail-> returns true for correct email', () => {
+    expect(isProperEmail('faizaan@gmail.com')).toBeTruthy();
+  });
+  test('isProperEmail-> returns false for in-correct email', () => {
+    expect(isProperEmail('faizaan@gmailcom')).toBeFalsy();
+  });
+});
+
+describe('Tests for isProperPass function', () => {
+  test('isProperPass-> returns true for correct password', () => {
+    expect(isProperPass('K@1qwert')).toBeTruthy();
+  });
+  test('isProperPass-> returns false for in-correct password', () => {
+    expect(isProperPass('K@qwertyu')).toBeFalsy(); // missing digit
+    expect(isProperPass('K1qwertyu')).toBeFalsy(); // missing special character
+    expect(isProperPass('1@qwertyu')).toBeFalsy(); // misssing capital letter
+    expect(isProperPass('K@1qwe')).toBeFalsy(); // min length should be 8
   });
 });
