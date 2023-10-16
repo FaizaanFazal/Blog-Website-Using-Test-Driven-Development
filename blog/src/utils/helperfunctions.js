638ff0efcb10a11029d9a16fba81ae5e6cc9bf72
Faizaan <dkfaizaan12@gmail.com>
2023-10-16 04:53:02 -0700

    updated regualr expression for isProperPass function

--- blog/src/utils/helperfunctions.js
+++ blog/src/utils/helperfunctions.js
@@ -15,7 +15,7 @@ export const isProperEmail = (email) => {
   return emailRegex.test(email);
 };
 export const isProperPass = (pass) => {
-  const passregex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/;
+  const passregex = /^(?=.*[A-Z]).(?=.*[0-9]).(?=.*[^a-zA-Z0-9]).{8,}$/;
   return passregex.test(pass);
 };
 export function backspace(element) {
