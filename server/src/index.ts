a1571df61b6a2e79a81fba6666640b859e18c95c
FaizaanFazal <dkfaizaan12@gmail.com>
2023-10-19 05:09:22 -0700

    old tsconfig

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,4 +1,14 @@
+import moduleAlias from 'module-alias';
+moduleAlias.addAlias('@', __dirname);
+// moduleAlias.addPath(__dirname + '/src')
+moduleAlias();
+
 import express, { Express, Request, Response } from "express";
+import {add} from '@/utils/helper';
+
+const result = add(1, 2);
+console.log(result)
+
 const port =8000;
  const app: Express=express();
 
@@ -7,5 +17,5 @@ const port =8000;
  })
 
 app.listen(port,()=>{
-    console.log(`now listning on port ${port}`);
+    console.log(`now listning on port ${port} and result is ${result}`);
 })
