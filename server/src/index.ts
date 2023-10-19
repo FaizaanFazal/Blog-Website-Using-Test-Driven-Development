5a11cb40c9b7e6d84c7a50a0765ed7ee7220b1c8
FaizaanFazal <dkfaizaan12@gmail.com>
2023-10-19 03:07:58 -0700

    Added express/ setted scripts

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,4 +1,11 @@
-import * as fs from 'fs';
+import express, { Express, Request, Response } from "express";
+const port =8000;
+ const app: Express=express();
 
-fs;
+ app.get("/",(req:Request, res:Response)=>{
+    res.send("Hello express running +type script");
+ })
 
+app.listen(port,()=>{
+    console.log(`now listning on port ${port}`);
+})
