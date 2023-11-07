de11cd7eccceea46f3a9f7ed177384c5b319a8e4
Faizaan <dkfaizaan12@gmail.com>
2023-11-07 17:51:08 +0500

    solved name rendering issue

--- blog/src/components/Navbar/Menuwrapper.jsx
+++ blog/src/components/Navbar/Menuwrapper.jsx
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import Navitem from './Navitem';
 import { useSelector } from 'react-redux';
 
 export default function Menuwrapper() {
-  const user =useSelector(state=>state.user.user); //userName
+  const [user, setUser] = useState();
+  const userr = useSelector(state => state.user.user); //userName
+  console.log(userr)
+
+  useEffect(() => {
+    if (userr["userName"] && userr["userName"].length > 0) {
+      setUser(userr);
+    }
+    else {
+      setUser(null)
+    }
+  }, [userr])
 
   return (
     <div>
@@ -16,11 +27,13 @@ export default function Menuwrapper() {
         <Navitem to="/about" text="About" />
         <Navitem to="/contacts" text="Contact" />
         <div className="nav-btns">
-        {user.userName ? 
+          {user ? (
             <div className="nav-btn btn">{user.userName}</div>
-        :
-        <Link data-testid="listitem" to="/login" className="nav-btn btn">Login</Link>
-        }
+          ) : (
+            <Link data-testid="listitem" to="/login" className="nav-btn btn">
+              Login
+            </Link>
+          )}
         </div>
         <div className="nav-btns">
           <Link data-testid="listitem" to="/createpost" className="nav-btn btn">Post</Link>
