3fa4c6a843f6aa174420385b4441652ad920688e
Faizaan <dkfaizaan12@gmail.com>
2023-11-13 21:17:05 +0500

    working on CardWide component

--- blog/src/components/Featured/Featured.jsx
+++ blog/src/components/Featured/Featured.jsx
@@ -1,15 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import CardWide from './CardWide';
 
 export default function Featured() {
-  const featured = useSelector((state) => state.blog.featured);
+  const blogs = useSelector((state) => state.blog.blogs);
+  const filteredPost = blogs.filter((blog) => blog.featured === true);
+  
 
   return (
     <div data-testid="featured" className="container">
       <section className="featured-sc bg-white">
-        {featured.map((blogItem) => (<CardWide blogItemData={blogItem} key={blogItem.id} />))}
-
+        {filteredPost.map((blogItem) => (<CardWide blogItemData={blogItem} key={blogItem.id} />))}
       </section>
     </div>
   );
