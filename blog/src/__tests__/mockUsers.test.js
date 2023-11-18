321bf5e014675e1d78e795df0a5a71ce10ced57c
Faizaan <dkfaizaan12@gmail.com>
2023-11-18 13:41:34 +0500

    working on login api mock test

--- blog/src/__tests__/mockUsers.test.js
+++ blog/src/__tests__/mockUsers.test.js
@@ -1,5 +1,8 @@
+import { fireEvent, screen } from '@testing-library/react';
+import Login from '../components/Login/Login';
 import {server} from '../mocks/server'
 import { rest } from 'msw';
+import { renderWithProviders } from '../utils/wrappertesting';
 
 
 beforeAll(() => server.listen());
@@ -14,7 +17,27 @@ describe('Mocking Users API', () => {
               return res(ctx.status(200));
             })
           )
+    });
 
+    it('Login Success', async() => {
+        renderWithProviders(<Login />);
+        const email= screen.getByTestId('inputEmail')
+        const pass= screen.getByTestId('inputPass')
+        const submitButton= screen.getByTestId('loginbtn')
+
+        fireEvent.change(email, { target: { value: 'dog@gmail.com' } });
+        fireEvent.change(pass, { target: { value: 'Qwe@1234' } });
+        fireEvent.click(submitButton);
+        screen.debug();
+    });
+
+    it('Login failure', async() => {
+        server.use(
+            rest.post("http://localhost:8000/users/login",(req,res,ctx)=>{
+              return res(ctx.status(401));
+            })
+          )
+          screen.debug();
     });
 
 });
--- blog/src/mocks/handler.js
+++ blog/src/mocks/handler.js
@@ -1,6 +1,6 @@
 import { rest } from 'msw';
 export const handlers = [
-  //todo
+  //get all users
   rest.get('http://localhost:8000/users/allusers', (req, res, ctx) => {
     return res(
       ctx.status(200),
@@ -20,5 +20,29 @@ export const handlers = [
     )
   }),
 
+  //login
+  rest.post('http://localhost:8000/users/login', (req, res, ctx) => {
+    if (req.body.username === 'dog@gmail.com' && req.body.password === 'Qwe@1234') {
+      return res(
+        ctx.json({
+          id: "de2f4aad-949e-4452-8799-aa09630b8e02",
+          email: "dog@gmail.com",
+          userName: "dog",
+          verified: false,
+          authorImageSrc: "http:example.com/author1",
+          authorImageAlt: "Author image",
+          createdAt: "2023-11-07T11:53:58.630Z",
+          updatedAt: "2023-11-07T11:53:58.630Z"
+        })
+      );
+    } else {
+      return res(
+        ctx.status(401),
+        ctx.json({
+          error: 'Invalid credentials',
+        })
+      );
+    }
+  })
 
 ]
